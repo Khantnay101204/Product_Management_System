aca18cc0e5c8c929a38141cd60fe3084898984b5
khantnaythwayzaw <khantnay55555@gmail.com>
2025-04-11 03:10:25 +0630

    Commit

--- src/main/java/ProductManagementSystem/Main.java
+++ src/main/java/ProductManagementSystem/Main.java
@@ -2,7 +2,7 @@ package ProductManagementSystem;
 
 import java.util.LinkedList;
 
-public class Test {
+public class Main {
     public static void main(String[] args) {
         String csv="Data/products dataset.csv";
         MainMenu main= MainMenu.getInstance();
